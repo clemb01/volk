2e97095a62cadfa74ef8748df5999db08ff8c4d2
clemb01 <32745478+clemb01@users.noreply.github.com>
2022-11-17 15:59:44 +0100

    Create premake5.lua

---
+++ premake5.lua
@@ -0,0 +1,44 @@
+project "Volk"
+	kind "StaticLib"
+	language "C"
+	staticruntime "off"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"volk.h",
+		"volk.c"
+	}
+
+	filter "system:linux"
+		pic "On"
+
+		systemversion "latest"
+
+		defines
+		{
+			"VK_USE_PLATFORM_XLIB_KHR"
+		}
+
+	filter "system:windows"
+		systemversion "latest"
+
+		defines 
+		{ 
+			"VK_USE_PLATFORM_WIN32_KHR"
+		}
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
+
+	filter "configurations:Dist"
+		runtime "Release"
+		optimize "on"
+    symbols "off"
